ef45404e69f4c5e82b91f4e9622aa9907619b374
leisyarcia <leisyarcia@gmail.com>
2021-03-01 19:46:59 +0000

    working

--- src/app.js
+++ src/app.js
@@ -6,6 +6,59 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  renderBalloons();
+};
+
+const container = document.querySelector(".container");
+let balloons = [
+  "pink",
+  "palegreen",
+  "powderblue",
+  "greenyellow",
+  "lightsalmon",
+  "aquamarine",
+  "seagreen",
+  "darkblue",
+  "crimson",
+  "springgreen",
+  "darkcyan",
+  "deeppink",
+  "dodgerblue",
+  "lawngreen",
+  "bisque",
+  "darkblue",
+  "pink",
+  "lightsalmon",
+  "red",
+  "purple",
+  "yellow",
+  "darkcyan",
+  "gold",
+  "palegreen",
+  "darkblue",
+  "aquamarine",
+  "crimson",
+  "lightsalmon"
+];
+
+function popBalloon(pos) {
+  balloons[pos] = null;
+  renderBalloons();
+}
+
+const renderBalloons = () => {
+  let content = "";
+  balloons.forEach((color, position) => {
+    content += `<div 
+        class="balloon ${color === null ? "popped" : "visibled"}"
+        style="background: ${color}">
+        </div>`;
+  });
+  container.innerHTML = content;
+  if (content !== "") {
+    let allBalloons = document.querySelectorAll(".balloon");
+    allBalloons.forEach((singleBalloon, index) => {
+      singleBalloon.addEventListener("click", () => popBalloon(index));
+    });
+  }
 };
